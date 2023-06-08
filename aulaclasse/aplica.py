17b6e6269585334d030ffc08322528be3610df61
EC2 Default User <ec2-user@ip-172-31-50-229.ec2.internal>
2023-06-08 00:52:36 +0000

    em progresso

--- aulaclasse/aplica.py
+++ aulaclasse/aplica.py
@@ -1,7 +1,5 @@
-from classes.GerenciarS3 import GerenciarS3
+from classes.MenuGerenciadorS3 import MenuGerenciadorS3
 
-controla_s3 = GerenciarS3('aulanoitejonatas788')
-
-diretorio_destino = 'orientacoes_objetos/aulaclasse/arquivos'
-nome_arquivo = 'fotinho.jpg'
-controla_s3.download_arquivo(nome_arquivo, diretorio_destino)
+nome_bucket = 'aulanoitejonatas788'
+menu = MenuGerenciadorS3(nome_bucket)
+menu.executar()
